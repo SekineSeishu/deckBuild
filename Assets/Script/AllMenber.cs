5c27771fa5511efef961db58675ac3f53a338286
SekineSeishu <mika242401@gmail.com>
2024-07-31 11:51:40 +0900

    チームセーブ機能追加

--- Assets/Script/AllMenber.cs
+++ Assets/Script/AllMenber.cs
@@ -7,6 +7,7 @@ public class AllMenber : MonoBehaviour
 {
     public int allCost;
     public TMP_Text costText;
+    public TMP_Text saveText;
     public List<Menber> menberList;
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,30 @@ public class AllMenber : MonoBehaviour
     void Update()
     {
         costText.text = "Cost:" + allCost;
+        if (allCost > 10)
+        {
+            costText.color = Color.red;
+        }
+    }
+
+    public void saveButton()
+    {
+        if (allCost > 10)
+        {
+            saveText.text = "CostOver!";
+            saveText.color = Color.red;
+        }
+        else if (allCost == 0)
+        {
+            saveText.text = "notMenber";
+            saveText.color = Color.red;
+        }
+        else
+        {
+            saveText.text = "saveComplete!";
+            saveText.color = Color.green;
+        }
+
     }
 
     public void GetmenberCost()
